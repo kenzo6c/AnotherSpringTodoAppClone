23244bdb6740bc7ee5a173651ccc6bb30ebac47c
kenzo6c <kenzo.carneiro.36@gmail.com>
2023-02-01 19:19:31 +0100

    Initial Commit

---
+++ src/main/java/com/kc36/todoAppClone/TodoAppCloneApplication.java
@@ -0,0 +1,13 @@
+package com.kc36.todoAppClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAppCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAppCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kc36/todoAppClone/TodoAppCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kc36.todoAppClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
