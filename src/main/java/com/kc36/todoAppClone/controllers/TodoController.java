e26648fadbbf88321a09f39335840a233ceb8ada
kenzo6c <kenzo.carneiro.36@gmail.com>
2023-02-03 00:18:30 +0100

    Added option to check or delete a todo item

--- src/main/java/com/kc36/todoAppClone/controllers/TodoController.java
+++ src/main/java/com/kc36/todoAppClone/controllers/TodoController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -22,17 +23,44 @@ public class TodoController {
     }
 
     @GetMapping("/")
-    public ModelAndView index() {
-        ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("todoItems", todoItemService.getAll());
-        modelAndView.addObject("todoItem", new TodoItem());
-        return modelAndView;
+    public String index(Model model) {
+        model.addAttribute("todoItems", todoItemService.getAll());
+        model.addAttribute("todoItem", new TodoItem());
+        model.addAttribute("edit", false);
+        return "index";
     }
 
     @PostMapping("/todoitem")
-    public String createTodoItem(@Valid TodoItem todoItem, BindingResult result, Model model)
+    public String saveTodoItem(@Valid TodoItem todoItem, BindingResult result, Model model)
     {
         System.out.println("new todoItem from the request:" + todoItem);
+        todoItemService.save(todoItem);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteTodoItem(@PathVariable Long id, Model model)
+    {
+        TodoItem todoItem = todoItemService.getById(id).orElseThrow(() -> new IllegalArgumentException("Todo item id "+ id + " not found."));
+        todoItemService.delete(todoItem);
+        return "redirect:/";
+    }
+
+//    @GetMapping("/edit/{id}")
+//    public String showEditForm(@PathVariable Long id, Model model)
+//    {
+//        System.out.println("edit val2: " + model.getAttribute("edit"));
+//        model.addAttribute("editedId", id);
+//        model.addAttribute("edit", true);
+//        System.out.println("edit val3: " + model.getAttribute("edit"));
+//        return "redirect:/";
+//    }
+
+    @GetMapping("/check/{id}")
+    public String checkTodoItem(@PathVariable Long id, Model model)
+    {
+        TodoItem todoItem = todoItemService.getById(id).orElseThrow(() -> new IllegalArgumentException("Todo item id "+ id + " not found."));
+        todoItem.setIsDone(!todoItem.getIsDone());
         todoItemService.save(todoItem);
         return "redirect:/";
     }
